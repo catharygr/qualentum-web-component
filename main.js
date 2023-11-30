e7711060ce3765ec7ce2c3d426fc77f63c46125d
catharygr <103951447+catharygr@users.noreply.github.com>
2023-11-30 17:35:00 +0000

    Repasando web component

---
+++ main.js
@@ -0,0 +1,77 @@
+class MyComponent extends HTMLElement {
+  parrafo = "Hola mundo";
+  myAtribute = "";
+
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+    this.myAtribute = this.getAttribute("my-attribute");
+  }
+
+  connectedCallback() {
+    this.render();
+  }
+
+  render() {
+    const { shadowRoot } = this;
+    shadowRoot.innerHTML = "";
+    shadowRoot.appendChild(this.htmlElement().content);
+  }
+
+  htmlElement() {
+    const pAttribute = this.myAtribute
+      ? `my-attribute="${this.myAtribute}"`
+      : "";
+    const html = `
+  <style>
+  .container {
+    display: block;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+  }
+  
+  p {
+ 
+    color: maroon;
+    font-size: 3rem;
+    margin: 0 auto;
+
+  }
+
+  button {
+    background-color: maroon;
+    color: white;
+    border: none;
+    border-radius: 5px;
+    padding: 1rem;
+    font-size: 1.5rem;
+    cursor: pointer;
+  }
+
+  button:hover {
+    background-color: #b30000;
+    box-shadow: 0 0 10px #b30000;
+  }
+  
+  
+  </style>
+
+<div class="container">
+    <p ${pAttribute}>
+    <slot name="text-parrafo">"${this.parrafo}"</slot>
+    <button>
+    <slot name="button"></slot>
+    </button>
+    </p>
+</div>
+
+  
+  `;
+    const template = document.createElement("template");
+    template.innerHTML = html;
+    return template;
+  }
+}
+
+window.customElements.define("my-component", MyComponent);
